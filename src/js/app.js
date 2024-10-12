ae77d41752b03a2afeb34e40d2574b2e393905f6
Henrique <henbarros694@gmail.com>
2024-10-12 11:58:42 -0300

    Função da color

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+function imgSlider(e) {
+    document.querySelector('#phone').src = e;
+}
+
+function circleChange(color) {
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
